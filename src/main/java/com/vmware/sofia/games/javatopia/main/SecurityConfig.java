5c7d3b8b8b9730339e29432aba56458393b6f344
Ilko Dragoev <ilko.dragoev@gmail.com>
2015-02-28 19:18:46 +0200

    Password can be set with -Dpassword on commandline when starting the server.

--- src/main/java/com/vmware/sofia/games/javatopia/main/SecurityConfig.java
+++ src/main/java/com/vmware/sofia/games/javatopia/main/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.vmware.sofia.games.javatopia.main;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,17 +12,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+   @Value("${password:password}")
+   private String password;
+
    @Autowired
    public void configureGlobal(AuthenticationManagerBuilder auth)
          throws Exception {
-      auth.inMemoryAuthentication().withUser("user").password("password")
-            .roles("USER");
+      auth.inMemoryAuthentication().withUser("user").password(password)
+      .roles("USER");
    }
 
    @Override
    protected void configure(HttpSecurity http) throws Exception {
       http.authorizeRequests()
-            .antMatchers("/api/secured/restart", "/api/secured/stop")
-            .authenticated().and().formLogin().and().httpBasic();
+      .antMatchers("/api/secured/restart", "/api/secured/stop")
+      .authenticated().and().formLogin().and().httpBasic();
    }
 }
